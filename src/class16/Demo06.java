d11c6e047f93a31f1b3c374349355ecbc9a13710
wang <1123813203@qq.com>
2019-06-10 09:26:51 +0800

    十六周周一

---
+++ src/class16/Demo06.java
@@ -0,0 +1,24 @@
+package class16;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Demo06 {
+    public static void main(String[] args) {
+        String path = "d:\\wang\\Test";
+        File file2 = new File(path);
+        if (!file2.exists()) {
+            file2.mkdirs();
+        }
+        String fileName = "abc.txt";
+        File file = new File(path, fileName);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
